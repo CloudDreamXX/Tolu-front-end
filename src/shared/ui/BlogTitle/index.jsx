0488c433e825768dfed2c2d93596fa293e43ea20
designindev <2438599@gmail.com>
2025-03-21 14:33:28 +0200

    fix breadcrumbs ui & conditions

--- src/shared/ui/BlogTitle/index.jsx
+++ src/shared/ui/BlogTitle/index.jsx
@@ -1,26 +1,14 @@
-import { Link } from 'react-router-dom';
 import { Title } from "../Title";
 import { TbEdit } from "react-icons/tb";
+import Breadcrumbs from "./Breadcrumbs";
 
-function BlogTitle({ title, description, breadcrumbs }) {
+function BlogTitle({ title, description, breadcrumbs }) { 
+
   return (
     <div className="flex flex-col items-start gap-2">
-        {breadcrumbs.length > 0 && (
-            <nav aria-label="breadcrumb">
-                <ol className="flex items-center gap-2">
-                    {breadcrumbs.map((crumb, index) => (
-                        <li key={index} className="flex items-center gap-2">
-                            {crumb.path ? (
-                                <Link to={crumb.path} className="text-blue-500">{crumb.name}</Link>
-                            ) : (
-                                <span>{crumb.name}</span>
-                            )}
-                            {index < breadcrumbs.length - 1 && <span>/</span>}
-                        </li>
-                    ))}
-                </ol>
-            </nav>
-        )}
+        {breadcrumbs.length > 0 && title !== "Published Content" &&
+            <Breadcrumbs breadcrumbs={breadcrumbs} />
+        }
         <div className="flex items-center gap-2">
             <TbEdit className="w-9 h-9 cursor-pointer" />
             <Title title={title} />
