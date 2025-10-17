ce6aca46298b8560f8c1e78af5935cd54960fcee
CloudDreamXX <clouddreamxx@gmail.com>
2025-10-17 16:05:42 +0300

    fix error

--- src/widgets/auth-forms/ui/login/ui.tsx
+++ src/widgets/auth-forms/ui/login/ui.tsx
@@ -90,11 +90,11 @@ export const LoginForm = () => {
     handleSend();
   }, [clientEmail]);
 
-  useEffect(() => {
-    localStorage.clear();
-    localStorage.removeItem("persist:user");
-    localStorage.removeItem("persist:clientMood");
-  }, [loginSchema]);
+  // useEffect(() => {
+  //   localStorage.clear();
+  //   localStorage.removeItem("persist:user");
+  //   localStorage.removeItem("persist:clientMood");
+  // }, [loginSchema]);
 
   const getOnboardingStatusWithRetry = async (attempt = 1) => {
     try {
@@ -355,11 +355,10 @@ export const LoginForm = () => {
                   name="email"
                   value={formData.email}
                   onChange={formDataChangeHandler}
-                  className={`px-[16px] py-[11px] h-[44px] rounded-[8px] w-full ${
-                    loginError
+                  className={`px-[16px] py-[11px] h-[44px] rounded-[8px] w-full ${loginError
                       ? "border border-[#FF1F0F]"
                       : "border border-[#DFDFDF]"
-                  }`}
+                    }`}
                 />
                 {loginError && (
                   <p className="text-[#FF1F0F] text-[14px]">{loginError}</p>
@@ -378,11 +377,10 @@ export const LoginForm = () => {
                     placeholder="Enter Password"
                     name="password"
                     onChange={formDataChangeHandler}
-                    className={`w-full px-[16px] py-[11px] h-[44px] rounded-[8px] ${
-                      passwordError
+                    className={`w-full px-[16px] py-[11px] h-[44px] rounded-[8px] ${passwordError
                         ? "border border-[#FF1F0F]"
                         : "border border-[#DFDFDF] focus:border-[#1C63DB]"
-                    }`}
+                      }`}
                   />
                   {formData.password && (
                     <button
@@ -473,11 +471,10 @@ export const LoginForm = () => {
           <div className="flex flex-col items-center gap-[24px] w-full">
             {isInvitedClient ? (
               <button
-                className={`w-full md:w-[250px] h-[44px] p-[16px] rounded-full flex items-center justify-center text-[16px] font-semibold ${
-                  formData.email && !loginError
+                className={`w-full md:w-[250px] h-[44px] p-[16px] rounded-full flex items-center justify-center text-[16px] font-semibold ${formData.email && !loginError
                     ? "bg-[#1C63DB] text-white"
                     : "bg-[#D5DAE2] text-[#5F5F65]"
-                }`}
+                  }`}
                 onClick={handleRequestInvite}
               >
                 Request invite
@@ -485,12 +482,11 @@ export const LoginForm = () => {
             ) : (
               <button
                 type="submit"
-                className={`w-full md:w-[250px] h-[44px] p-[16px] rounded-full flex items-center justify-center text-[16px] font-semibold ${
-                  (!isCodeSent && formData.email) ||
-                  (isCodeSent && formData.code)
+                className={`w-full md:w-[250px] h-[44px] p-[16px] rounded-full flex items-center justify-center text-[16px] font-semibold ${(!isCodeSent && formData.email) ||
+                    (isCodeSent && formData.code)
                     ? "bg-[#1C63DB] text-white"
                     : "bg-[#D5DAE2] text-[#5F5F65]"
-                }`}
+                  }`}
               >
                 {loginMode === "2fa"
                   ? isCodeSent
