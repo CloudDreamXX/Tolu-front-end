47e987480b91c2d13d5feb05a5f86b52f797e8f3
CloudDreamXX <clouddreamxx@gmail.com>
2026-02-06 10:22:29 +0200

    fixes

--- src/pages/profile/components/Field/Field.tsx
+++ src/pages/profile/components/Field/Field.tsx
@@ -3,7 +3,7 @@ export const Field = ({ label, value }: { label: string; value: string }) => (
     <p className="text-[#5F5F65] text-[12px] md:text-[18px] font-[500]">
       {label}
     </p>
-    <p className="text-[#1D1D1F] text-[14px] md:text-[20px] font-[500]">
+    <p className="text-[#1D1D1F] text-[14px] md:text-[20px] font-[500] break-words whitespace-pre-wrap">
       {value}
     </p>
   </div>
